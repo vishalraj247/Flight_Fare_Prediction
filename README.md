62cfd4d68905e5a15bc93d57b4778f9bfd7d6ceb
Vishal Raj <vishalraj247@gmail.com>
2023-10-16 02:45:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flight_Fare_Prediction
+ A data product developed in Streamlit using Neural Networks, that will help users in the USA to better estimate their local travel airfare.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data product developed in Streamlit using Neural Networks, that will help users in the USA to better estimate their local travel airfare.',
+    author='Vishal Raj',
+    license='MIT',
+)
